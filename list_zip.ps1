85988f55bab4d274fc544bb7549f660c7e50e2ff
esdutiii <esdutiii@users.noreply.github.com>
2026-06-10 14:33:23 +0200

    Clean up repository from legacy estupalant folders, aux scripts and txt files

--- list_zip.ps1
+++
@@ -1,5 +0,0 @@
-Add-Type -AssemblyName System.IO.Compression
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-$z = [System.IO.Compression.ZipFile]::OpenRead($args[0])
-$z.Entries | Select-Object -First 15 -Property FullName
-$z.Dispose()
